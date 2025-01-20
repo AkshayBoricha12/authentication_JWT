199dc0cdd238db0aa033b712ac78712c222fb7f2
AkshayBoricha12 <akshay.maxah@gmail.com>
2025-01-20 19:03:04 +0530

    completed frontend

---
+++ client/authentication/src/App.jsx
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- server/controllers/Login.js
+++ server/controllers/Login.js
@@ -12,12 +12,15 @@ const login = async (req, res) => {
     return res.status(400).json({ message: "All input is required" });
   }
   const user = await User.findOne({ email });
+  console.log("user:", user);
   if (!(user && (await bcrypt.compare(password, user.password)))) {
     return res.status(404).json({ message: "Invalid credentials" });
   }
-  const token = createSecretToken(user._id);
+  // const token = createSecretToken(user._id);
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGU0NWQ1N2U3MWI1MjIzZGM1OGI4YyIsImlhdCI6MTczNzM3NzY4MywiZXhwIjoxNzM3NjM2ODgzfQ.bCrDKcZcXhrWLrc-TfMzK1PpRCtuetvg1MhE613z5Uk
+`;
+
   res.cookie("token", token, {
-    domain: process.env.FRONTEND_URL, // Set your domain here
     path: "/", // Cookie is accessible from all paths
     expires: new Date(Date.now() + 86400000), // Cookie expires in 1 day
     secure: true, // Cookie will only be sent over HTTPS
